60d18f12293dfa11ca80b9da030e89316b1453b8
justerik <sebo.erik@gmail.com>
2026-04-16 18:06:06 +0200

    Initial commit for Forge 1.19.2 port

---
+++ src/main/java/net/withrage/chpcore/CHPcore.java
@@ -0,0 +1,32 @@
+package net.withrage.chpcore;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.withrage.chpcore.messages.custom.PlayerJoinHandler;
+import org.slf4j.Logger;
+
+@Mod(CHPcore.MODID)
+public class CHPcore
+{
+    public static final String MODID = "chpcore";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public CHPcore(FMLJavaModLoadingContext context)
+    {
+        IEventBus modEventBus = context.getModEventBus();
+        MinecraftForge.EVENT_BUS.register(this);
+        MinecraftForge.EVENT_BUS.register(PlayerJoinHandler.class);
+    }
+}
