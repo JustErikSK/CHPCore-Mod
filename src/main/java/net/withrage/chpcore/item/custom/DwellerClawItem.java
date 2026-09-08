a9c088e4fbc15ed9c9c69a7dd8242d9ca2275975
justerik <sebo.erik@gmail.com>
2026-09-08 15:52:23 +0200

    dweller claw now gives a really short wither effect

--- src/main/java/net/withrage/chpcore/item/custom/DwellerClawItem.java
+++ src/main/java/net/withrage/chpcore/item/custom/DwellerClawItem.java
@@ -1,5 +1,9 @@
 package net.withrage.chpcore.item.custom;
 
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
 
@@ -7,5 +11,16 @@ public class DwellerClawItem extends SwordItem {
 
     public DwellerClawItem(Tier tier, int attackDamageModifier, float attackSpeedModifier, Properties properties) {
         super(tier, attackDamageModifier, attackSpeedModifier, properties);
+    }
+
+    @Override
+    public boolean hurtEnemy(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        target.addEffect(new MobEffectInstance(
+                MobEffects.WITHER,
+                50,
+                0
+        ));
+
+        return super.hurtEnemy(stack, target, attacker);
     }
 }
